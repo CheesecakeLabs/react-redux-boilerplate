75ed0bc9310ef039e95e7e318b7c13dfa218858a
Gabriel Neutzling <gabrielneutzling@gmail.com>
2016-07-25 10:44:39 -0300

    Add dependencies and server configs.

---
+++ server.js
@@ -0,0 +1,31 @@
+/* eslint no-console: 0 */
+const path = require('path');
+const express = require('express');
+const webpack = require('webpack');
+const config = require('./webpack.config');
+const port = process.env.PORT || 3000;
+
+const app = express();
+const compiler = webpack(config);
+
+app.use(require('webpack-dev-middleware')(compiler, {
+  publicPath: config.output.publicPath,
+  stats: {
+    colors: true,
+  },
+}));
+
+app.use(require('webpack-hot-middleware')(compiler));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'src/index.html'));
+});
+
+app.listen(port, 'localhost', (err) => {
+  if (err) {
+    console.log(err);
+    return;
+  }
+
+  console.log(`Listening at http://localhost:${port}`);
+});
