4b3b4dac1917a424c7cd26837ee2709a17b78400
enapupe <enapupe@gmail.com>
2017-02-22 11:45:30 -0300

    add define action package

--- src/modules/counter/actions.js
+++ src/modules/counter/actions.js
@@ -1,5 +1,7 @@
-export const INCREMENT_COUNTER = 'INCREMENT_COUNTER'
-export const DECREMENT_COUNTER = 'DECREMENT_COUNTER'
+import { defineAction } from '../../utils/define-action'
+
+export const INCREMENT_COUNTER = defineAction('INCREMENT_COUNTER')
+export const DECREMENT_COUNTER = defineAction('DECREMENT_COUNTER')
 
 export const incrementCounter = () => ({
   type: INCREMENT_COUNTER,
