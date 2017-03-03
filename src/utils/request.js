f7dde4113d607e6ea6a8a3151e0da0355bcf17e1
enapupe <enapupe@gmail.com>
2017-03-03 18:42:11 -0300

    change request default to removeTrailingSlash

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,3 +1,3 @@
 import Fetch from '@ckldeveloper/fetch'
 
-export default new Fetch('https://api.github.com/')
+export default new Fetch('https://api.github.com/', null, { removeTrailingSlash: true })
