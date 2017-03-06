14dbef28e9decda7c46468ee35ca7ec6a52cc893
Daniel Leite de Oliveira <dleitee@gmail.com>
2017-03-06 16:51:53 -0300

    add error and loading reducers

--- src/modules/reducers.js
+++ src/modules/reducers.js
@@ -2,11 +2,15 @@ import { combineReducers } from 'redux'
 import { routerReducer as routing } from 'react-router-redux'
 
 import { user, member } from './user/reducers'
+import { error } from './error/reducers'
+import { loading } from './loading/reducers'
 
 const rootReducer = combineReducers({
   routing,
   user,
   member,
+  error,
+  loading,
 })
 
 export default rootReducer
