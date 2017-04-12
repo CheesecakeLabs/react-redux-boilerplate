73667efbecbe6285ea07832ca5b684c968f3218b
enapupe <enapupe@gmail.com>
2017-04-12 10:59:36 -0300

    improve server.dev output

--- server.js
+++ server.js
@@ -6,15 +6,14 @@ const webpackHotMiddleware = require('webpack-hot-middleware')
 const config = require('./webpack.config')
 const baseHTML = require('./src/index.html')
 
+const ip = '0.0.0.0'
 const port = process.env.PORT || 3000
 const app = express()
 const compiler = webpack(config)
 
 app.use(webpackDevMiddleware(compiler, {
   publicPath: config.output.publicPath,
-  stats: {
-    colors: true,
-  },
+  stats: 'errors-only',
 }))
 
 app.use(webpackHotMiddleware(compiler))
@@ -30,11 +29,11 @@ app.get('*', (req, res) => {
   res.send(baseHTML())
 })
 
-app.listen(port, '0.0.0.0', (err) => {
+app.listen(port, ip, (err) => {
   if (err) {
     console.warn(err)
     return
   }
 
-  console.info(`Listening at http://0.0.0.0:${port}`)
+  console.info('\x1b[32m', `[Development] Express is running on http://${ip}:${port}`, '\x1b[0m')
 })
