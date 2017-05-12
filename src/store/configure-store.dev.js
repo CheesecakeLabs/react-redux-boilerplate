8ce3ff32db2d1f5a70e9bd3fa3175c1b88e83c42
enapupe <enapupe@gmail.com>
2017-05-12 15:10:40 -0300

    plug REDUX_DEVTOOLS_EXTENSION hook to be used with chrome extension

--- src/store/configure-store.dev.js
+++ src/store/configure-store.dev.js
@@ -30,6 +30,8 @@ const configureStore = (preloadedState) => {
         router,
         logger,
       ),
+      // eslint-disable-next-line no-underscore-dangle
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
     ),
   )
 
